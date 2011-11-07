806f1b646155da4e0cfd52571d2ae615a882e8ae
Bryan Donovan <bdondo@gmail.com>
2011-11-07 00:42:44 -0800

    indenting got messed up again, not sure why

--- server/sfw/page.rb
+++ server/sfw/page.rb
@@ -2,62 +2,62 @@ require 'json'
 require File.expand_path("../random_id", __FILE__)
 
 module Sfw
-	class PageError < StandardError; end;
+  class PageError < StandardError; end;
 
-	# Page Class
-	# Handles writing and reading JSON data to and from files.
-	class Page
-		class << self
-			# Directory where pages are to be stored.
-			attr_accessor :directory
-			# Directory where default (pre-existing) pages are stored.
-			attr_accessor :default_directory
+  # Page Class
+  # Handles writing and reading JSON data to and from files.
+  class Page
+    class << self
+      # Directory where pages are to be stored.
+      attr_accessor :directory
+      # Directory where default (pre-existing) pages are stored.
+      attr_accessor :default_directory
 
-			# Get a page
-			#
-			# @param [String] name - The name of the file to retrieve, relative to Page.directory.
-			# @return [Hash] The contents of the retrieved page (parsed JSON).
-			def get(name)
-				assert_directories_set
+      # Get a page
+      #
+      # @param [String] name - The name of the file to retrieve, relative to Page.directory.
+      # @return [Hash] The contents of the retrieved page (parsed JSON).
+      def get(name)
+        assert_directories_set
 
-				path = File.join(directory, name)
+        path = File.join(directory, name)
 
-				if File.exist? path
-					load_and_parse path
-				else
-					default_path = File.join(default_directory, name)
+        if File.exist? path
+          load_and_parse path
+        else
+          default_path = File.join(default_directory, name)
 
-					if File.exist?(default_path)
-						FileUtils.mkdir_p File.dirname(path)
-						FileUtils.cp default_path, path
-						load_and_parse path
-					else
-						put name, {'title'=>name,'story'=>[{'type'=>'factory', 'id'=>Sfw::RandomId.generate}]} unless File.file? path
-					end
-				end
-			end
+          if File.exist?(default_path)
+            FileUtils.mkdir_p File.dirname(path)
+            FileUtils.cp default_path, path
+            load_and_parse path
+          else
+            put name, {'title'=>name,'story'=>[{'type'=>'factory', 'id'=>Sfw::RandomId.generate}]} unless File.file? path
+          end
+        end
+      end
 
-			# Create or update a page
-			#
-			# @param [String] name - The name of the file to create/update, relative to Page.directory.
-			# @param [Hash] page - The page data to be written to the file (it will be converted to JSON).
-			# @return [Hash] The contents of the retrieved page (parsed JSON).
-			def put(name, page)
-				assert_directories_set
-				File.open(File.join(directory, name), 'w') { |file| file.write(JSON.pretty_generate(page)) }
-				page
-			end
+      # Create or update a page
+      #
+      # @param [String] name - The name of the file to create/update, relative to Page.directory.
+      # @param [Hash] page - The page data to be written to the file (it will be converted to JSON).
+      # @return [Hash] The contents of the retrieved page (parsed JSON).
+      def put(name, page)
+        assert_directories_set
+        File.open(File.join(directory, name), 'w') { |file| file.write(JSON.pretty_generate(page)) }
+        page
+      end
 
-			private
+      private
 
-			def load_and_parse(path)
-				JSON.parse(File.read(path))
-			end
+      def load_and_parse(path)
+        JSON.parse(File.read(path))
+      end
 
-			def assert_directories_set
-				raise PageError.new('Sfw::Page.directory must be set') unless directory
-				raise PageError.new('Sfw::Page.default_directory must be set') unless default_directory
-			end
-		end
-	end
+      def assert_directories_set
+        raise PageError.new('Sfw::Page.directory must be set') unless directory
+        raise PageError.new('Sfw::Page.default_directory must be set') unless default_directory
+      end
+    end
+  end
 end
